77424c29c51166aa976af0924d7ba18590c71e06
Robert Hofer <robert.hofer@docfinder.at>
2017-11-07 15:45:37 +0100

    db config

--- app/environments/dev/common/config/main-local.php
+++ app/environments/dev/common/config/main-local.php
@@ -1,20 +1,30 @@
 <?php
-return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
+
+return (function () {
+
+    $host = getenv('POSTGRES_HOST');
+    $db = getenv('POSTGRES_DB');
+    $user = getenv('POSTGRES_USER');
+    $pass = getenv('POSTGRES_PASSWORD');
+    $port = getenv('POSTGRES_PORT');
+
+    return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => "pgsql:host=$host;port=$port;dbname=$db",
+                'username' => $user,
+                'password' => $pass,
+                'charset' => 'utf8',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'viewPath' => '@common/mail',
+                // send all mails to a file by default. You have to set
+                // 'useFileTransport' to false and configure a transport
+                // for the mailer to send real emails.
+                'useFileTransport' => true,
+            ],
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-    ],
-];
+    ];
+})();
